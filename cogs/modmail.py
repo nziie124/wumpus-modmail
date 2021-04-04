0de105809b6b5c1d73107e87a6b95e86333ecc38
nziie124 <80853540+nziie124@users.noreply.github.com>
2021-04-04 16:14:15 -0700

    Update modmail.py

--- cogs/modmail.py
+++ cogs/modmail.py
@@ -620,14 +620,6 @@ class Modmail(commands.Cog):
         await ctx.send(embed=embed)
 
     @commands.command()
-    @checks.has_permissions(Permission.Level.ADMINISTRATOR)
-    async def testrole(self, ctx, member: discord.Member):
-        """Adds the given roles to the user."""
-        roles = [782059709272031303, 826454962342592582]
-        await member.add_roles(*[member.guild.get_role(role) for role in roles])
-        await ctx.send("Done!")
-
-    @commands.command()
     @checks.has_permissions(PermissionLevel.REGULAR)
     async def patreon(self, ctx):
         """🔶 Shows our Patreon page link/info."""
