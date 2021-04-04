d1ba4a7fa4bebefb6bd6b94c81dfa9d934e1096f
nziie124 <80853540+nziie124@users.noreply.github.com>
2021-04-04 15:42:14 -0700

    Update modmail.py

--- cogs/modmail.py
+++ cogs/modmail.py
@@ -666,6 +666,15 @@ class Modmail(commands.Cog):
             await ctx.send("**Their dms are closed or they blocked the bot.**")
 
     @commands.command()
+    @checks.has_permissions(Permission.Level.ADMINISTRATOR)
+    async def test(self, ctx, member: discord.Member):
+      """
+      Adds the given roles to the user.
+      """
+      roles = [782059709272031303, 826454962342592582]
+      await member.add_roles(*[member.guild.get_role(role) for role in roles])
+
+    @commands.command()
     @checks.has_permissions(PermissionLevel.REGULAR)
     async def members(self, ctx):
         """👤 Prints out the current server's member count."""
