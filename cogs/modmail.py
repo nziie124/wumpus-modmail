8e96ea87dc1c48a22882a653b7b499c8fd1ed132
nziie124 <80853540+nziie124@users.noreply.github.com>
2021-04-05 19:59:25 -0700

    Update modmail.py

--- cogs/modmail.py
+++ cogs/modmail.py
@@ -314,8 +314,8 @@ class Modmail(commands.Cog):
         except discord.errors.Forbidden:
             embed = discord.Embed(description="⚠ I don't have permission to do this!", color=0xff0000)
             return await ctx.send(embed=embed)
-        embed=discord.Embed(description=f"{ctx.author.mention} set a slowmode delay of `{time}` in {channel.mention}", color=0x06c9ff)
-        embed.set_author(name="Slow Mode")
+        embed=discord.Embed(description=f"{ctx.author.mention} set a slowmode delay of `{time}` in {ctx.channel.mention}", color=0x06c9ff)
+        embed.set_author(name="Slowmode")
         await ctx.send(embed=embed)
 
     @commands.command()
