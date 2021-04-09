a0a43aa3fb0d6548662a510f43fb61bec8882593
nziie124 <80853540+nziie124@users.noreply.github.com>
2021-04-09 07:53:59 -0700

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -269,6 +269,20 @@ class Utility(commands.Cog):
     def cog_unload(self):
         self.bot.help_command = self._original_help_command
 
+    @commands.Cog.listener()
+    async def on_member_join(self, member: discord.Member):
+        if member.guild.id != 777720307334512670:
+            return
+        embed = discord.Embed(
+            title="Welcome!",
+            description=f"Welcome to the server {member.mention}! - This welcome message is currently being writen and is not finished yet. Please bare with us! `-` All the information about this server should have been sent to you via DMs by <@!155149108183695360>. Soon, all that information will be on this message.",
+            timestamp=datetime.utcnow(),
+            color=discord.Colour.random(),
+    )   
+        embed.set_footer(text=f"👤 User ID: {member.id}", icon_url=member.avatar_url)
+        embed.set_thumbnail(url=member.avatar_url)
+        await self.bot.get_channel(777720307540295718).send(content=f"**Welcome {member.mention}!**", embed=embed)
+
     @commands.command(aliases=["info"])
     @checks.has_permissions(PermissionLevel.OWNER)
     @utils.trigger_typing
@@ -2055,3 +2069,4 @@ class Utility(commands.Cog):
 
 def setup(bot):
     bot.add_cog(Utility(bot))
+
